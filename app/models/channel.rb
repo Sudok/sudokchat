e08bca5245a9899dfbf7d20d8989882a856e1f2a
Sudok <glauber.gw@gmail.com>
2018-01-11 22:55:38 -0200

    creating models

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -2,4 +2,5 @@ class Channel < ApplicationRecord
   has_many :messages, as: :messagable, :dependent => :destroy
   belongs_to :user
   belongs_to :team
+  validates_presence_of :slug, :team, :user
 end
